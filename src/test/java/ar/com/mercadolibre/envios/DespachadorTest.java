f5e764ff4076d241281a80ac0371b09018b0a8fd
Manuel Ponsa <mponsa@ar0fvfym1cjhv2j.ml.com>
2019-08-13 11:09:14 -0300

    Gradle build

--- src/test/java/ar/com/mercadolibre/envios/DespachadorTest.java
+++ src/test/java/ar/com/mercadolibre/envios/DespachadorTest.java
@@ -9,7 +9,8 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.*;
+
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class DespachadorTest {
